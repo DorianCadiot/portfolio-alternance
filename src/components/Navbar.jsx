e82556095e535a3bb78983ef991a3b5d09c4e7ce
DorianCadiot <167541051+DorianCadiot@users.noreply.github.com>
2025-04-17 13:37:48 +0200

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const NavItems = () => {
             <motion.a
               href={href}
               className="text-lg md:text-base text-white"
-              whileHover={{ color: "#ff9720" }}
+              whileHover={{ color: "#09c800ff" }}
               transition={{ duration: 0.2, ease: "easeOut" }}
             >
               {name}
@@ -61,7 +61,8 @@ const Navbar = () => {
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
           isScrolled || isOpen
             ? "bg-[#1a191e] border-b-2 border-[#00bdd9ff] border-opacity-100 shadow-md"
-            : "bg-transparent border-b-2 border-[#00bdd9ff] border-opacity-0"
+            : "bg-transparent border-b-0"
+            //: "bg-transparent border-b-2 border-[#00bdd9ff] border-opacity-0"
         }`}
       >
         <div className="mx-auto px-6 sm:px-8 md:px-10">
